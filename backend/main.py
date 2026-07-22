4b7e891be5b5da86958dcbbd899a6101fbed08fe
nayan-nikam <nayannikam4@gmail.com>
2026-07-22 13:57:08 +0530

    CORS Origin allowed

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Indian Movie Recommender API")
 # 3. Configure CORS for React
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
